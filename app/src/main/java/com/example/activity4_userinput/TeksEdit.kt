92319cde15907e69d5c799ce14439f8229c9f532
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-31 17:42:58 +0700

    menambah variabel untuk menampilkan data yang telah di input

--- app/src/main/java/com/example/activity4_userinput/TeksEdit.kt
+++ app/src/main/java/com/example/activity4_userinput/TeksEdit.kt
@@ -1,2 +1,29 @@
 package com.example.activity4_userinput
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.material3.HorizontalDivider as Divider
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun FormDataDiri(modifier: Modifier){
+    //Variabel untuk mengingat nilai masukan dari keyboard
+
+    var textNama by remember {mutableStateOf("")}
+    var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
+
+    //variabel untuk menyimpan data yang diperoleh dari kompenen UI
+    var nama by remember {mutableStateOf("")}
+    var alamat by remember {mutableStateOf("")}
+    var jenis by remember {mutableStateOf("")}
+
+
+}
